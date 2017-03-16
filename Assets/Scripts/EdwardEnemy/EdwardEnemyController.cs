b6c20072b6fad95004820cf8ac2915fb3706baf1
edward <hughes_personal@yahoo.com>
2017-03-16 00:13:52 -0500

    Edward's EnemyController

---
+++ Assets/Scripts/EdwardEnemy/EdwardEnemyController.cs
@@ -0,0 +1,37 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+enum EnemyStates { Idle, Patrol, Attack };
+
+public class EdwardEnemyController : MonoBehaviour {
+
+    public GameObject Creature;
+    public List<GameObject> PatrolNodes;
+
+    private Animation CreatureAnimation;
+    private EnemyStates EnemyState;
+    private GameObject StartNode;
+
+	void Start () {
+        EnemyState = EnemyStates.Idle;
+
+        StartNode = new GameObject("Patrol Start Location");
+        StartNode.GetComponent<Transform>().position = new Vector3(Creature.transform.position.x, Creature.transform.position.y, Creature.transform.position.z);
+        PatrolNodes.Insert(0, StartNode);
+	}
+	
+	void Update () {
+        if(EnemyState == EnemyStates.Idle)
+        {
+            Creature.GetComponent<Animation>().Play("creature1Idle");
+        }
+        //Debug.Log(PatrolNodes[0]);
+
+        //EnemyAnimation = Enemy.GetComponent<Rigidbody>().GetComponentInParent<Animation>();
+        //EnemyAnimation.Play("creature1Idle");
+
+        //EnemyAnimation.Play("creature1walkforward");
+        //Enemy.GetComponent<Animation>().Play("creature1walkforward");
+    }
+}
