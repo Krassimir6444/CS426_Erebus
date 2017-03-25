53312ebc5a17605862e8529127358dd9551b1d5c
edward <hughes5683@gmail.com>
2017-03-25 05:43:07 -0500

    Demo Helper script
    script only serves us during in class demo.
    allows us to set things like player's speed and flashlight battery
    count.

---
+++ Assets/HacksOnBabyxD.cs
@@ -0,0 +1,51 @@
+﻿/* === Hacks On Baby xD ===
+ * Used during demo only.
+ * Allow us to change player settings so we can speed through the level as 
+ * well as give/get resources without actually going to specific locations 
+ * where the resources reside in the map.
+ * */
+
+/* Note to Self:
+ *     RigidbodyFirstPersonController:
+ *         ForwardSpeed, BackwardSpeed, StrafeSpeed
+ *         Run Multiplier
+ *     PlayerHealth:
+ *         CurrentHealth
+ *     PlayerStamina:
+ *         CurrentStamina
+ *     PlayerInventory:
+ *         Has
+ *             Fist
+ *             Flashlight
+ *             Crowbar
+ *             LaserPistol
+ *             Keycard
+ *         Equipped
+ *             Fist
+ *             Flashlight
+ *             Crowbar
+ *             LaserPistol
+ *          NumMedkits
+ *          NumBatteries
+ *      Under PlayerPrefab:
+ *          Player->PlayerView->Equipment:
+ *              HandLight
+ *              Crowbar
+ *          HUD (low priority)
+ * */
+using System;
+using UnityEngine;
+
+public class HacksOnBabyxD : MonoBehaviour {
+    public class PlayerValues
+    {
+
+    }
+	void Start () {
+		
+	}
+	
+	void Update () {
+		
+	}
+}
